2a7cc2f235aa99d7791afb0d2f1b53ff6dbd0d5c
thvanarkel <me@thomasvanarkel.nl>
2020-03-31 14:50:03 +0200

    First implementation of chatbot

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const botgram = require("botgram")
 const bot = botgram("1095782983:AAFw5ojhwHt6YyQK-Y93vEJ9bPT2DLLq71A")
 
+var cron = require('node-cron');
+
+
+
 console.log("Started")
 
-bot.command("start", "help", (msg, reply) =>
-  reply.text("To schedule an alert, do: /alert <seconds> <text>"))
+bot.command("start", "help", (msg, reply) => {
+  reply.text("Hi there! I'm a bot that will help you take charge of your bedtime routines! Are you ready to embark on a journey?")
+  reply.text("To schedule an alert, do: /alert <seconds> <text>")
+  console.log(msg)
+})
 
 bot.command("alert", (msg, reply, next) => {
   var [ seconds, text ] = msg.args(2)
@@ -13,12 +20,26 @@ bot.command("alert", (msg, reply, next) => {
   setTimeout(() => reply.text(text), Number(seconds) * 1000)
 })
 
+var keyboard1 = [[  "🚀" ], [ "Beam me up!" ]];
+
 bot.text(function (msg, reply, next) {
   console.log("Received a text message:", msg.text);
-  reply.text("hello!");
-  reply.markdown("Here's some _good_ sticker:");
-  reply.sticker("BQADAgAD3gAD9HsZAAFphGBFqImfGAI");
+  reply.keyboard(keyboard1, false).text("hello!");
 });
 
 bot.command((msg, reply) =>
   reply.text("Invalid command."))
+
+
+// bot.reply(1083726752).text("HIYAA")
+
+
+var task = cron.schedule('0 10 18 * * *', () =>  {
+  bot.reply(1083726752).text("HIYAA");
+  console.log(this)
+  task.destroy()
+}, {
+  scheduled: true
+});
+
+// task.start();
